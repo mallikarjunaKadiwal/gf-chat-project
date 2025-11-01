cfdfb7f9f4427727afbd5c00f50033cb429c224f
mallikarjunaKadiwal <mallikarjunakadiwal86@gmail.com>
2025-11-01 23:25:54 +0530

    Open CORS to all origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -53,11 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "https://my-chat-app-gf4z.onrender.com",
-]
+CORS_ALLOWED_ORIGINS = True
 
 ROOT_URLCONF = 'backend.urls'
 
